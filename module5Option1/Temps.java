3dfd20d263df91175b311af3863abc8795bc1247
Braden Durian <braden.durian@csuglobal.edu>
2024-01-21 19:31:46 -0700

    third commit

--- module5Option1/Temps.java
+++ module5Option1/Temps.java
@@ -8,11 +8,11 @@ public class Temps {
 	public static void main(String[] args) {
 		
 		//Declare Variables
-		ArrayList<Double> dailyTemp = new ArrayList<Double>();
-		ArrayList<String> days = new ArrayList<String>();
-		double weeklyAvg = 0;
-		String userInput;
-		int currIndex;
+		ArrayList<Double> dailyTemp = new ArrayList<Double>(); //ArrayList for daily average temperatures
+		ArrayList<String> days = new ArrayList<String>(); //ArrayList for Days of the Week
+		float weeklyAvg = 0; //float for calculating the weekly average temperature
+		String userInput; //String for holding userInput
+		int currIndex; //Current Index, int for holding the index in days of user input
 		
 		//Add daily temps to dailyTemp from Monday 15JAN24 to Sunday 21JAN24
 		dailyTemp.add(-1.46);
@@ -38,18 +38,71 @@ public class Temps {
 			weeklyAvg += temp;
 		}
 		
-		//Get average temp for the week
+		//Get average temp for the week by dividing the sum of daily temperatures by 7
 		weeklyAvg = weeklyAvg / 7;
 		
 		//Create Scanner
 		Scanner scnr = new Scanner(System.in);
 		
-		//Prompt User
-		System.out.println("Enter a day of the week to view the average temperature.\n Or enter 'week' to see the all days\n and the average for the week");
 		
-		//Test
-		for(double temp : dailyTemp) {System.out.println(temp);}
-		for(String day : days) {System.out.println(day);}
+		//Prompt User
+		System.out.println("Enter a day of the week to view the average temperature.\nOr enter 'week' to see "
+				+ "the average for each day and the \naverage for the week. \nEnter 'exit' to close the program");
+		
+		
+		//Get user input, trim whitespace, and make it upper case to match the days ArrayList. 
+		userInput = scnr.nextLine();
+		userInput = userInput.trim();
+		userInput = userInput.toUpperCase();
+		
+		//Keep program running until user chooses to exit
+		while(!userInput.equals("EXIT")) {
+			//Get index of the entered day
+			currIndex = days.indexOf(userInput);
+			
+			//Handle bad inputs. Any input other than 'week' or 'exit' should have a non-negative index. 
+			while(currIndex == -1 && (!(userInput.equalsIgnoreCase("week") || userInput.equalsIgnoreCase("exit")))) {
+				
+				System.out.println(userInput + " is not a valid entry. Please try again.");
+				userInput = scnr.nextLine();
+				userInput = userInput.trim();
+				userInput = userInput.toUpperCase();
+				currIndex = days.indexOf(userInput);
+			}
+			
+			//Print data for user if day of the week requested
+			if(currIndex != -1) {
+				
+				System.out.println(days.get(currIndex) + " Average Temperature: " + dailyTemp.get(currIndex));
+			}
+			
+			//Print data for user if week requested
+			else {
+				
+				for(int i = 0; i < days.size(); i++) {
+					
+					System.out.println(days.get(i) + " Average Temperature: " + dailyTemp.get(i));
+				}
+				System.out.printf("Average Temperature for the Week: %.2f\n", weeklyAvg);
+				
+			}
+			
+			//next iteration
+			userInput = scnr.nextLine();
+			userInput = userInput.trim();
+			userInput = userInput.toUpperCase();
+			
+			
+			//Test ArrayLists
+			//for(double temp : dailyTemp) {System.out.println(temp);}
+			//for(String day : days) {System.out.println(day);}
+		}
+		//close Scanner
+		scnr.close();
+		
+		//Terminate Program
+		return;
+
 	}
 
 }
