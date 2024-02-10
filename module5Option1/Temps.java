d63c84ea82fc9f641449b310d7515a3aeaea7fd2
Braden Durian <braden.durian@csuglobal.edu>
2024-02-09 17:00:10 -0700

    Portfolio Corrections

--- module5Option1/Temps.java
+++ module5Option1/Temps.java
@@ -14,14 +14,8 @@ public class Temps {
 		String userInput; //String for holding userInput
 		int currIndex; //Current Index, int for holding the index in days of user input
 		
-		//Add daily temps to dailyTemp from Monday 15JAN24 to Sunday 21JAN24
-		dailyTemp.add(-1.46);
-		dailyTemp.add(13.63);
-		dailyTemp.add(41.0);
-		dailyTemp.add(33.65);
-		dailyTemp.add(16.67);
-		dailyTemp.add(26.67);
-		dailyTemp.add(43.12);
+		//Use custom method for updating daily temperatures
+		updateTempsForWeek(dailyTemp, -1.46, 13.63, 41.0, 33.65, 16.67, 26.67, 43.12);
 		
 		//Add days of the week to days
 		days.add("MONDAY");
@@ -32,10 +26,11 @@ public class Temps {
 		days.add("SATURDAY");
 		days.add("SUNDAY");
 		
-		//Add daily temps together
+		//Add daily temps together using foreach loop. 
 		for(double temp : dailyTemp) {
 			
 			weeklyAvg += temp;
+			
 		}
 		
 		//Get average temp for the week by dividing the sum of daily temperatures by 7
@@ -45,22 +40,27 @@ public class Temps {
 		Scanner scnr = new Scanner(System.in);
 		
 		
-		//Prompt User
+		//Prompt user to enter a command recognized by the program. Next lines are used to improve readability in terminal
 		System.out.println("Enter a day of the week to view the average temperature.\nOr enter 'week' to see "
-				+ "the average for each day and the \naverage for the week. \nEnter 'exit' to close the program");
+		+ "the average for each day and the \naverage for the week. \nEnter 'exit' to close the program");
 		
 		
-		//Get user input, trim whitespace, and make it upper case to match the days ArrayList. 
+		//Get user input, trim whitespace, and make it upper case to match the days ArrayList.
 		userInput = scnr.nextLine();
 		userInput = userInput.trim();
 		userInput = userInput.toUpperCase();
 		
 		//Keep program running until user chooses to exit
 		while(!userInput.equals("EXIT")) {
+			
 			//Get index of the entered day
 			currIndex = days.indexOf(userInput);
 			
-			//Handle bad inputs. Any input other than 'week' or 'exit' should have a non-negative index. 
+			/*Check if the index in days of userInput is equal to -1 AND not "week" or "exit". 
+			 *Any valid input other than 'week' or 'exit' should have a non-negative index
+			 *If the loop condition is met, inform the user of invalid input and prompt for new input
+			 *Then process input in the same fashion as above
+			 */
 			while(currIndex == -1 && (!(userInput.equalsIgnoreCase("week") || userInput.equalsIgnoreCase("exit")))) {
 				
 				System.out.println(userInput + " is not a valid entry. Please try again.");
@@ -68,12 +68,14 @@ public class Temps {
 				userInput = userInput.trim();
 				userInput = userInput.toUpperCase();
 				currIndex = days.indexOf(userInput);
+				
 			}
 			
 			//Print data for user if day of the week requested
 			if(currIndex != -1) {
 				
 				System.out.println(days.get(currIndex) + " Average Temperature: " + dailyTemp.get(currIndex));
+				
 			}
 			
 			//Print data for user if week requested
@@ -82,7 +84,9 @@ public class Temps {
 				for(int i = 0; i < days.size(); i++) {
 					
 					System.out.println(days.get(i) + " Average Temperature: " + dailyTemp.get(i));
+					
 				}
+				
 				System.out.printf("Average Temperature for the Week: %.2f\n", weeklyAvg);
 				
 			}
@@ -92,11 +96,8 @@ public class Temps {
 			userInput = userInput.trim();
 			userInput = userInput.toUpperCase();
 			
-			
-			//Test ArrayLists
-			//for(double temp : dailyTemp) {System.out.println(temp);}
-			//for(String day : days) {System.out.println(day);}
 		}
+		
 		//close Scanner
 		scnr.close();
 		
@@ -104,5 +105,18 @@ public class Temps {
 		return;
 
 	}
+	
+	//custom method for adding daily temperatures to an ArrayList. This method improves readability in the main method.z
+	public static void updateTempsForWeek(ArrayList<Double> array, double mon, double tue, double wed, double thurs, double fri, 
+			double sat, double sun) {
+		array.add(mon);
+		array.add(tue);
+		array.add(wed);
+		array.add(thurs);
+		array.add(fri);
+		array.add(sat);
+		array.add(sun);
+	}
+	
 
 }
